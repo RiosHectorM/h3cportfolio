80452d06917fd9d85c560d9d4b7fbdf0cd6da418
RiosHectorM <rios.hector.martin@gmail.com>
2023-09-20 18:51:18 -0300

    'change name'

--- app/head.js
+++ app/head.js
@@ -1,8 +1,8 @@
 const Head = () => (
   <>
-    <title>H3C-Dev Portfolio</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/favicon.png" />
+    <title>Hector Martin Rios Portfolio</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <link rel='icon' href='/favicon.png' />
   </>
 );
 export default Head;
